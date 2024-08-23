73657b0aacd20a70439d9358003e74c634eaa9c1
PedroCordova <olympic.spop@outlook.com>
2024-08-23 16:34:17 -0300

    Atualização do Projeto

--- Assets/sprites/slime.gd
+++ Assets/sprites/slime.gd
@@ -3,10 +3,20 @@ extends Node2D
 const SPEED = 60
 
 var direction = 1
+var health: int = 100
 
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite = $AnimatedSprite2D
+
+func die():
+	queue_free()
+
+
+func take_damage(damage_amount: int):
+	health -= damage_amount
+	if health <= 0:
+		die()
 
 
 func _process(delta):
--- Scripts/fire_ball.gd
+++ Scripts/fire_ball.gd
@@ -1,23 +1,13 @@
 extends Area2D
 
 const SPEED = 220
-var damage: int = 100
 var direction = 1
-var lifetime: float = 2.0
 var velocity = Vector2()
 
-
-func _on_body_entered(body):
-	if body.is_in_group("enemies"):
-		body.take_damage(damage)
-		queue_free()
 
 
 func _ready():
 	pass
-	
-	
-
 
 func _physics_process(delta):
 	velocity.x  = SPEED * delta * direction
